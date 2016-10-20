15d9659fb341aa096bf8f07ec5f978f7f8904ebd
Gaetitan <gaetitan@gmail.com>
2016-10-20 17:48:42 +0800

    MazeDaoImplTest changes
    Nothing new, just starting new tests

--- src/test/model/dao/MazeDaoImplTest.java
+++ src/test/model/dao/MazeDaoImplTest.java
@@ -5,7 +5,18 @@ import org.junit.Test;
 public class MazeDaoImplTest {
 
 	@Test
-	public void createMazeTest() {
+	public void getMazeByNameTest() {
+		/*String name = "MuscuMaze";
+		Maze maze = DataBaseFacade.getMazeByName(name);
+		
+		assertNotNull(maze);
+		assertEquals(maze.getId(), Integer.valueOf(2));
+		assertEquals(maze.getName(), "MuscuMaze");
+		assertEquals(maze.getLength(), Integer.valueOf(45));
+		assertEquals(maze.getWidth(), Integer.valueOf(25));
+		assertEquals(maze.getCreationDate(), java.sql.Date.valueOf("2016-10-09"));
+		assertEquals(maze.getCreator().getId(), Integer.valueOf(2));
+		assertEquals(maze.getCreator().getName(), "Benji");*/
 	}
 
 }
