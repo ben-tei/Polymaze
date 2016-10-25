9d098cf05fde9e77969a1e64691ef9428dc45001
Rodolphe Brossard <rodobros@hotmail.com>
2016-10-25 14:36:53 +0800

    eller strategy test

---
+++ src/test/model/factory/strategies/EllerStrategyTest.java
@@ -0,0 +1,30 @@
+package test.model.factory.strategies;
+
+import org.junit.Test;
+
+import model.business.Maze;
+import model.business.MazeSolver;
+import model.business.Person;
+import model.factory.strategies.EllerStrategy;
+import util.exception.PolymazeException;;
+
+/**
+ * @author Rodobros TODO
+ */
+public class EllerStrategyTest
+{
+
+	@Test
+	public void testEllerStrategy1()
+	{
+		EllerStrategy eller = new EllerStrategy();
+		MazeSolver solver = new MazeSolver();
+		Maze mazeToSolve = eller.generateMaze("Test Eller1", 50, 50, new Person(999, "rb"));
+		try {
+			solver.solveMaze(mazeToSolve);
+		} catch (PolymazeException e) {
+			// test failed!
+		}
+	}
+
+}
